a21430892e8adef2fdd458df99b3145e3d9faea7
Brian Gerhards <briangerhards@gmail.com>
2020-02-26 00:08:02 -0600

    first commit

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+        }
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Debug.Log(s);
+        s.source.Play();
+    }
+}
---
+++ Assets/HareController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HareController : MonoBehaviour
+{
+    public string trackName;
+    public GameObject next;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play(trackName);
+        if(next) next.SetActive(true);
+        GameObject.Destroy(this.gameObject);
+    }
+}
