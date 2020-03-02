07c5f8c045ac09e548241fa776c2e36b2e3bbc4a
Brian Gerhards <briangerhards@gmail.com>
2020-03-01 22:56:51 -0600

    only show joystick when mobile

---
+++ Assets/VirtualJoystickEnabler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class VirtualJoystickEnabler : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            gameObject.SetActive(true);
+        }
+            
+    }
+}
